ea4c669023e8fb355c1046c7edccd7b1f2ebec45
el glioui <elgliouiyoucode@gmail.com>
2024-01-10 15:38:26 +0100

    this commit for fixing homecontroller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,10 +1,25 @@
 <?php
+
 namespace App\Controllers;
-use Core\View;
+
+use Core\View; // Import the View trait
+
 class HomeController
 {
+    use View; 
+
     public function test()
     {
        
+        try {
+            $view = 'login'; 
+            $params = [
+                
+            ];
+
+            $this->render($view, $params);
+        } catch (\Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
     }
 }
